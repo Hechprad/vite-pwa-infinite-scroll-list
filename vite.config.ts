46bd96bb9a201d9140da16dd5b8ba303e8324600
Hecherat <hecherat@gmail.com>
2025-03-13 11:52:30 -0300

    feat: Add PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest/config" />
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -6,7 +7,57 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'vite-pwa-infinite-scroll-list',
+        short_name: 'InfiniteScroll',
+        theme_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/225x225.png',
+            sizes: '225x225',
+            type: 'image/png',
+          },
+        ],
+        screenshots: [
+          {
+            src: '/screenshots/desktop-1280x800.png',
+            sizes: '1280x800',
+            type: 'image/png',
+            form_factor: 'wide',
+          },
+          {
+            src: '/screenshots/mobile-750x1334.png',
+            sizes: '750x1334',
+            type: 'image/png',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        sourcemap: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
